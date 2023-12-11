f8e5528c36a8e8c73e6055e5508df61a8cf8e063
danieloka <danirc01@gmail.com>
2023-12-10 19:39:32 -0500

    Sprint 2 front-end completed

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -28,18 +28,21 @@ public class HomebankingApplication {
 			Client melba = new Client("Melba", "Morel", "melba@mindhub.com");
 			clientRepository.save(melba);
 			System.out.println(melba);
-
+//			Client clara = new Client("Clara", "Rodriguez", "crodriguez@aol.com");
+//			clientRepository.save(clara);
 
 
 			Account account001 = new Account("VIN-001", LocalDate.now(),5000.0);
 			Account account002 = new Account("VIN-002", LocalDate.now().plusDays(1),7500.0);
+			//Account account003 = new Account("VIN-003", LocalDate.now().plusDays(2), 7750.0);
 
 			melba.addAccount(account001);
 			melba.addAccount(account002); //Añade las cuentas al cliente
+			//clara.addAccount(account003);
 
 			accountRepository.save(account001);
 			accountRepository.save(account002); //Guarda las cuentas en la base de datos
-
+			//accountRepository.save(account003);
 
 
 			System.out.println(account002);
